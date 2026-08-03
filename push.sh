a1f09e23f5ce1fa02fc9f339511b3fa5858bfe5c
EndlessPixel <system-mini@outlook.com>
2026-08-03 22:52:48 +0800

    chore: 添加部署推送脚本

---
+++ push.sh
@@ -0,0 +1,4 @@
+sudo cp /home/system-mini/文档/Code/simple-local-music-player/play.html /mnt/music/
+sudo cp /home/system-mini/文档/Code/simple-local-music-player/style.css /mnt/music/
+sudo cp /home/system-mini/文档/Code/simple-local-music-player/script.js /mnt/music/
+sudo cp /home/system-mini/文档/Code/simple-local-music-player/server.js /mnt/music/
